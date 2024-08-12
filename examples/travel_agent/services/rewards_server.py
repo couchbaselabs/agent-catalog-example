33ae13656401a49ee496edab61bc0b84d5553ae5
Glenn Galvizo <glenn.galvizo@couchbase.com>
2024-08-12 15:45:58 -0700

    Annotating the rewards endpoints.

--- examples/travel_agent/services/rewards_server.py
+++ examples/travel_agent/services/rewards_server.py
@@ -1,27 +1,40 @@
 import random
 import uuid
 import fastapi
+import pydantic
 import datetime
 
 travel_server = fastapi.FastAPI()
 
 
+class NewMemberResponse(pydantic.BaseModel):
+    member_name: str
+    member_id: str
+
+
+class GetMemberRewardsResponse(pydantic.BaseModel):
+    class Rewards(pydantic.BaseModel):
+        points: int
+
+    member_id: str
+    member_since: str
+    rewards: Rewards
+
+
 @travel_server.post("/create")
-def create_new_member(member_name: str):
+def create_new_member(member_name: str) -> NewMemberResponse:
     """ Create a new travel-rewards member. """
-    return {
-        'member_name': member_name,
-        'member_id': uuid.uuid4().hex
-    }
+    return NewMemberResponse(
+        member_name=member_name,
+        member_id=uuid.uuid4().hex
+    )
 
 
 @travel_server.get("/rewards/{member_id}")
-def get_member_rewards(member_id: str):
+def get_member_rewards(member_id: str) -> GetMemberRewardsResponse:
     """ Get the rewards associated with a member. """
-    return {
-        'member_id': member_id,
-        'member_since': str((datetime.datetime.today() - datetime.timedelta(days=100)).isoformat()),
-        'rewards': {
-            'points': random.randint(1, 10000),
-        }
-    }
+    return GetMemberRewardsResponse(
+        member_id=member_id,
+        member_since=str((datetime.datetime.today() - datetime.timedelta(days=100)).isoformat()),
+        rewards=GetMemberRewardsResponse.Rewards(points=random.randint(1, 10000))
+    )
