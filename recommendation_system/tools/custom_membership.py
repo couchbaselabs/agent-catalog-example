95e45b8e14368938a1ef6059ce2a6a9b307e13b4
Nishanth VM <nishanth.vm@couchbase.com>
2024-11-20 23:10:15 +0530

    recommendation system sample app v1.4

--- recommendation_system/tools/custom_membership.py
+++ recommendation_system/tools/custom_membership.py
@@ -4,7 +4,7 @@ import re
 from agentc_core.tool import tool
 
 @tool
-def vector_search_compat(listA:list[str], listB:list[str]) -> str:
+def custom_membership_check(listA:list[str], listB:list[str]) -> str:
     """for given two list of words, select the one which is in both the list"""
     hashmap = {word: True for word in listA}
     
